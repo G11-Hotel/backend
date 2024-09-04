4eb5ca1c75566acb134c18f38045ac1ca6594608
Lorenzo Cortese <129780161+TheDeadGuy77@users.noreply.github.com>
2024-09-04 17:42:10 +0200

    Add files via upload

---
+++ routes/ricerca.ts
@@ -0,0 +1,36 @@
+
+import express from 'express';
+import Hotel from '../models/hotel';
+import Stanza from '../models/stanza';
+
+const ricercaRouter = express.Router();
+
+ricercaRouter.get('/ricerca', async (req,res)=>{
+    
+    let hotels = await Hotel.find({provincia : req.query.provincia});
+    if(!hotels){
+        res.status(404).json({message: "Not Found"});
+        return;
+    }
+    else{
+       let availableHotels: { _id: any; hotel: any; numeroStelle: any; }[] = [];
+       // waiting for operations in map function to complete. map iterates over hotels and for each hotel searches for available rooms
+       await Promise.all(hotels.map(async (hotel:any)=>{ 
+            const stanze = await Stanza.find({IDhotel: hotel._id, reserved: false, numeroPostiLetto: {$eq: req.query.numeroPersone} });
+            if(stanze.length > 0){
+                availableHotels.push({_id: hotel._id, hotel: hotel.nome, numeroStelle: hotel.numeroStelle});
+            }
+       }));
+
+       if(availableHotels.length > 0){
+            res.status(200).json({
+                message: "OK",
+                hotels: availableHotels
+            });
+       }
+       else{
+            res.status(404).json({message: "Not Found"});
+       }
+    }
+});
+export default ricercaRouter;
